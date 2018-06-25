a457bdf2976bc2508839a382e5fc50b1c6584c44
Jason Williams <uberlinuxguy@gmail.com>
2018-06-25 09:11:45 -0400

    Work on parse_packet() to help with troubleshooting and error handling.

--- mprov/utils.py
+++ mprov/utils.py
@@ -15,23 +15,30 @@ def parse_packet(data):
     :return: a dict with key=value pairs, or none if the packet is not parseable.
     :rtype: dict
     """
+    data = data.strip()
 
     tmp_dict = dict()
 
+
     tmp_dict["raw_packet"] = data
     if data == "":
-        return None
-    try:
-        data = data.strip()
-        for token in data.split():
-            if token.find("=") >= 0:
-                (key, value) = token.split("=")
-                tmp_dict[key] = value
-            else:
-                # no key=value, so the string at this token is considered a "key" with a None value.
-                tmp_dict[token] = None
+        return tmp_dict
+    if data is None:
+        return tmp_dict
 
-    except Exception as e:
-        print_err("Error: Unable to parse packet. Exception: " + e.message)
-        return None
+    #print "Parsing: #" + data + "#"
+    #try:
+
+    for token in data.split(" "):
+        #print str(token)
+        if token.find("=") >= 0:
+            (key, value) = token.split("=")
+            tmp_dict[key] = value
+        else:
+            # no key=value, so the string at this token is considered a "key" with a None value.
+            tmp_dict[token] = None
+
+    #except Exception as e:
+     #   print_err("Error: Unable to parse packet. Exception: " + str(e))
+      #  return None
     return tmp_dict
